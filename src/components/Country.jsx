3be888a16870a0db5410f3d7c0d21f142b7f31b0
Hubert <hubertblaszczyk03@gmail.com>
2022-08-31 20:23:02 +0200

    all functions

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,13 +1,97 @@
+import { useState } from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './styles/Country.css'
 
 function Country(props){
+    // function getFromDict(dict){
+    //     const data = Object.keys(dict).map(objKey =>{
+    //         return (
+    //             <div>{props.}</div>
+    //         )
+    //     })
+    // }
+    function getNative(){
+        // i dont need to map over it
+        const nativeNames = Object.keys(props.name.nativeName).map(objKey => {
+        
+            return (
+                <div>
+                    {props.name.nativeName[objKey].common}
+                </div>
+            )
+        })
+        return nativeNames[0]
+    }
+
+    function getCurrencies(){
+        const currs = Object.keys(props.currencies).map(objKey =>{
+            return(
+                <div>{props.currencies[objKey].name}</div>
+            )
+        })
+        return currs  
+    }
+    
+    function getLanguages(){
+        const langs = Object.keys(props.languages).map(objKey =>{
+            return <div>{props.languages[objKey]}</div>
+        })
+        return langs
+    }
     return (
-        <div className="country">
-            <img src={props.flag} alt={`flag of ${props.name}`} className='flag'/>
-            <h4 className="name-of-country">{props.name}</h4>
-            <div><span>Population: </span>{props.population}</div>
-            <div><span>Region:</span>{props.region}</div>
-            <div><span>Capital: </span>{props.capital}</div>
+        <div>
+            {props.isPicked ? 
+            <div className='single-country'>
+                <button className='back' onClick={props.unpicked}>
+                    {/* <FontAwesomeIcon icon={["fa-solid fa-arrow-left"]} /> */}
+                    Back
+                </button>
+                <img src={props.flags.svg} alt={`flag of ${props.name.common}`} className='flag'/>
+                <h3>{props.name.common}</h3>
+                    <div>
+                        <span>Native Name: </span>
+                        {props.name.nativeName ? getNative() : ''}
+                    </div>
+                    <div>
+                        <span>Population: </span>
+                        {props.population}
+                    </div>
+                    <div>
+                        <span>Region: </span>
+                        {props.region}
+                        </div>
+                    <div>
+                        <span>Sub Region: </span>
+                        {props.subregion}
+                    </div>
+                    <div>
+                        <span>Capital: </span>
+                        {props.capital}
+                    </div>
+                    <div>
+                        <span>Top Level Domain: </span>
+                        {props.domain}
+                    </div>
+                    <div>
+                        <span>Currencies: </span>
+                        {getCurrencies()}
+                    </div>
+                    <div>
+                        <span>Languages: </span>
+                        {getLanguages()}
+                    </div>
+                    <div>
+                </div>
+            </div>
+            :
+            <div className="country" onClick={() => {props.picked(props.name.common)}}>
+                <img src={props.flags.svg} alt={`flag of ${props.name.common}`} className='flag'/>
+                <h4 className="name-of-country">{props.name.common}</h4>
+                <div><span>Population: </span>{props.population}</div>
+                <div><span>Region:</span>{props.region}</div>
+                <div><span>Capital: </span>{props.capital}</div>
+            </div>
+        }
         </div>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,18 +2,26 @@ import {useState, useEffect} from 'react'
 import Country from './Country'
 import {nanoid} from 'nanoid'
 import './styles/Main.css'
-function Main(props){
+function Main(){
     const [countriesData, setCountriesData] = useState([])
     const [formData, setFormData] = useState({
       input: '',
       select: ''
     })
+    const [currentCountry, setCurrentCountry] = useState()
 
     useEffect(() =>{
         fetch('https://restcountries.com/v3.1/all')
           .then(response => response.json())
           .then(data => {
-            setCountriesData(data)
+            setCountriesData(() => {
+              return data.map(country => {
+                return{
+                  ...country,
+                  isPicked: false
+                }
+              })
+            })
           })
       }, [])
 
@@ -23,13 +31,15 @@ function Main(props){
       }
 
       function data(){
+        if(currentCountry){
+          return currentCountry
+        }
         if (formData.input !== '' || formData.select !== ''){
           return countriesData.filter(country => {
             const {name, region } = country
             if(name.common.toLowerCase().includes(formData.input.toLowerCase())
             && 
             (formData.select === ''|| region.toLowerCase() === formData.select)){
-              console.log('hih')
               return true
             }else{
               return false
@@ -40,33 +50,54 @@ function Main(props){
         }
       }
       
-      
+      function picked(nameNeeded){
+        setCurrentCountry(() => {
+          const current =  countriesData.filter(country => {
+            console.log(country)
+            const {name} = country
+            return name.common === nameNeeded ? true : false
+          })
+          return current.map(data => ({...data, isPicked: true}))
+        })
+      }
+
+      function unpicked(){
+        setCurrentCountry()
+      }
+
+      console.log(countriesData)
       
     
       const countries = data().map(country => {
-        const {flags, name, population, region, capital } = country
         return <Country
-            key={nanoid()} 
-            flag={flags.svg}
-            name={name.common}
-            population={population}
-            region={region}
-            capital={capital}
+            key={nanoid()}
+            domain={country.tld}
+            currencies={country.currencies}
+            languages={country.languages}
+            name={country.name}
+            flags={country.flags}
+            population={country.population}
+            region={country.region}
+            subregion={country.subregion}
+            capital={country.capital}
+            isPicked={country.isPicked}
+            picked={picked}
+            unpicked={unpicked}
         />
       })
 
     return(
         <main className={`main`}>
           <div className='form'>
-            <input type="search" placeholder='Search for a country...' onChange={filterCountries} />
-            <select defaultValue="" onChange={filterCountries}>
+            {!currentCountry && <input type="search" placeholder='Search for a country...' onChange={filterCountries} className='search-name'/>}
+            {!currentCountry && <select defaultValue="" onChange={filterCountries}>
               <option value="" disabled hidden>Filter by Region</option>
               <option value="africa">Africa</option>
               <option value="americas">Americas</option>
               <option value="asia">Asia</option>
               <option value="europe">Europe</option>
               <option value="oceania">Oceania</option>
-            </select>
+            </select>}
           </div>
           <div className='countries'>
             {countries}
